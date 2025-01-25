2f883660e09936b1ee05b1c35b8ff59359a9dbba
Jose  Lucas <joselucas787@hotmail.com>
2025-01-25 19:35:17 -0300

    tfz

--- Laboratorio 4/main.tf
+++ Laboratorio 4/main.tf
@@ -6,3 +6,106 @@ resource "local_file" "private_key_pen" {
     content = tls_private_key.generated.private_key_pen
     filename = "MyAWSKey.pen"
 }
+
+resource "aws_key_pair" "generated" {
+    key_name    =   "MyOtherAWSKey"
+    public_key = tls_private_key.generated.public_key_openssh
+
+    lifecycle {
+        ignore_changes = [key_name]
+    }
+}
+
+## Criando máquina virtual
+resource "aws_instance" "servidor_ubuntu"{
+    ami = data.aws_ami.ubuntu.id
+    instance_type   =   "t2.micro"
+    subnet_id   =   aws_subnet.public_subnet["public_subnet_1"].id
+    security_groups =   [aws_security_group.ingress-ssh.id]
+    associate_public_ip_address = true
+    key_name    = aws_key_pair.generated.key_name
+    ## Criando a conexão com o usuario jose
+    connection {
+    user        = "jose"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+    }
+
+    tags = {
+        name = "Ubuntu EC2 Server"
+    }
+    
+}
+
+## PERMITIR ACESSO SSH
+resource "aws_security_group" "ingress-ssh"{
+    name    =   "Permitir-SSH"
+    vpc_id  =   aws_vpc.vpc.vpc_id
+    ingress {
+        cidr_blocks = [
+            "0.0.0.0/0"
+        ]
+        from_port   =   22
+        to_port =   22
+        protocol    =   "tcp"
+    }
+
+    ## Permite tráfego de saída 
+    egress {
+        from_port   =   0
+        to_port     =   0
+        protocol    =   "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+## Criando um Security Group para Tráfego WEB
+
+resource "aws_security_group" "vpc-web" {
+    name    =   "vpc-web-${terraform.workspace}"
+    vpc_id  =   aws_vpc.vpc.id
+    description = "Web Traffic"
+    ingress{
+        description = "Permitir tráfego na porta 80"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks =   ["0.0.0.0/0"]
+    }
+    ingress{
+        description = "Permitir tráfego na porta 443"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks =   ["0.0.0.0/0"]
+    }
+    egress {
+        description = " Permitir todas os IPs e Portas de saída"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+## permitindo tráfego ICMP
+resource "aws_security_group" "vpc-ping" {
+    name    =   "vpc-ping"
+    vpc_id  =   aws_vpc.vpc.id
+    description =   "ICMP para PING"
+    ingress{
+        description =   "Permite tráfego ICMP"
+        from_port   =   -1
+        to_port     =   -1
+        protocol    =   "icmp"
+        cidr_blocks =   ["0.0.0.0/0"]
+    }
+    egress{
+        description = "Permite todos os IP's e Porta Outbound"
+        from_port   =   0
+        to_port     =   0
+        protocol    =   "-1"
+        cidr_blocks =   ["0.0.0.0/0"]
+    }
+}
+}
---
+++ Laboratorio 4/provider.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+  #access_key = "ajseijsaoijesaoij"
+  #secret_key = "oijeaoesaoikesapokeapopokspoakespo"
+  #é possível definir as credentials keys dentro do bloco de provider.  
+}
+
+#Para configurar as variáveis de ambiente da máquina é necessário definir da seguinte forma por meio de uma CLI do Windows.
+
+#$ export AWS_ACCESS_KEY_ID="anaccesskey"
+#$ export AWS_SECRET_ACCESS_KEY="asecretkey"
+#$ export AWS_DEFAULT_REGION="us-east-1"
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
