caadb858e0be17795c8bd576a241337531cc4713
Jose  Lucas <joselucas787@hotmail.com>
2025-01-29 09:50:32 -0300

    commit

--- Laboratorio 7/variables.tf
+++ Laboratorio 7/variables.tf
@@ -10,3 +10,32 @@ variable "count" {
 variable "enable" {
     type = bool
 }
+
+
+## Variavel Subnet
+variable "subnets" {
+    default =   {
+        public-1a   =   {
+            cidr_block      =   "10.42.100.0/24"
+            availability_zone   =   "us-east-1a"
+        }
+        public-1b   =   {
+            cidr_block      =   "10.42.101.0/24"
+            availability_zone   =   "us-east-1b"
+        }
+        private-1a  =   {
+            cidr_block      =   "10.42.200.0/24"
+            availability_zone   =  "us-east-1a"
+        }
+        private-1b  =   {
+            cidr_block      =   "10.42.202.0/24"
+            availability_zone   =   "us-east-1b"
+        }
+    }
+}
+
+description = "Subnets for the VPC"
+type =  map(object({
+    cidr_block      =   string
+    availability_zone=  string
+    }))
